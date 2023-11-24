1aa0644a9da6bdb65083027ef29ae0185dc88e8b
abuan.270094 <abuan.270094@LAB-A-PC08>
2023-11-24 17:34:38 +0800

    Add project files.

---
+++ BasicCalculator/FrmBasicCalculator.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace BasicCalculator
+{
+    partial class FrmBasicCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.cbOperations = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.result = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Basic Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.BlueViolet;
+            this.label2.Location = new System.Drawing.Point(12, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total:";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox1.Location = new System.Drawing.Point(25, 63);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(288, 30);
+            this.txtBox1.TabIndex = 2;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox2.Location = new System.Drawing.Point(25, 150);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(288, 30);
+            this.txtBox2.TabIndex = 3;
+            // 
+            // cbOperations
+            // 
+            this.cbOperations.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbOperations.FormattingEnabled = true;
+            this.cbOperations.Location = new System.Drawing.Point(139, 106);
+            this.cbOperations.Name = "cbOperations";
+            this.cbOperations.Size = new System.Drawing.Size(45, 32);
+            this.cbOperations.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.result);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(25, 202);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(288, 100);
+            this.panel1.TabIndex = 5;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.ForeColor = System.Drawing.Color.BlueViolet;
+            this.result.Location = new System.Drawing.Point(12, 38);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(67, 25);
+            this.result.TabIndex = 2;
+            this.result.Text = "00000";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(128, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Compute";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FrmBasicCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(346, 400);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cbOperations);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBasicCalculator";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmBasicCalculator_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.ComboBox cbOperations;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BasicCalculator/FrmBasicCalculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorPrivateAssembly;
+
+namespace BasicCalculator
+{
+    public partial class FrmBasicCalculator : Form
+    {
+        public FrmBasicCalculator()
+        {
+            InitializeComponent();
+        }
+        float num1, num2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num1 = float.Parse(txtBox1.Text);
+            num2 = float.Parse(txtBox2.Text);
+
+            if (cbOperations.Text == "+")
+            {
+               float total = BasicComputation.Addition(num1,num2);
+               result.Text = total.ToString();
+            }
+            else if (cbOperations.Text == "-")
+            {
+                float total = BasicComputation.Subtraction(num1,num2);
+                result.Text = total.ToString();
+            }
+            else if (cbOperations.Text == "*")
+            {
+                float total = BasicComputation.Multiplication(num1, num2);
+                result.Text = total.ToString();
+            }
+            else if (cbOperations.Text == "/")
+            {
+                float total = BasicComputation.Division(num1,num2);
+                result.Text = total.ToString();
+            }
+        }
+
+        private void FrmBasicCalculator_Load(object sender, EventArgs e)
+        {
+            cbOperations.Items.Add("+");
+            cbOperations.Items.Add("-");
+            cbOperations.Items.Add("*");
+            cbOperations.Items.Add("/");
+        }
+
+
+    }
+}
